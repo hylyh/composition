ca3a558a278657200c1c75b57d8e9d23dd986919
j hayley <me@hayley.zone>
2017-10-29 16:34:40 -0700

    Variablize push script

--- gh-pages-push.sh
+++ gh-pages-push.sh
@@ -1,10 +1,19 @@
 #!/bin/sh
 
+# Build and push just the build directory to the gh-pages branch
+# gh-pages branch must already exist
+# Warning: Force pushes to gh-pages every time,
+#  you shouldn't plan on working in that branch if you use this
+
 set -e
 
-pub build
-git add -f build/web
-git commit -m "Deploying to gh-pages"
-git push origin `git subtree split --prefix build/web master`:gh-pages --force
+BUILD="pub build"
+BUILDDIR="build/web"
 
+$BUILD
+git add -f $BUILDDIR
+git commit -m "Deploying to gh-pages"
+git push origin `git subtree split --prefix $BUILDDIR master`:gh-pages --force
+
+# Roll back the commit we made for the deploy (no working changes will be lost)
 git reset HEAD~1
